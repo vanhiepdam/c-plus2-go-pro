c50dd96fbeebce4da62f7a4a171842247057d4ac
vanhiepdam <vanhiepdam@gmail.com>
2019-12-25 10:22:13 +0700

    Update Abstraction.cpp
    add comment

--- Abstraction.cpp
+++ Abstraction.cpp
@@ -1,3 +1,4 @@
+// Khái niệm ở đây
 #include <iostream>
 
 using namespace std;
@@ -15,3 +16,9 @@ int main() {
     mystery mystery_box;
     cout << mystery_box.whats_in_the_box() << endl;
 }
+
+
+// 1. Cách đặt tên class - style: CamelCase - VD: (Person, HumunResource)
+// 2. Đây không phải là abstraction
+// 3. Với mỗi bài, nêu khái niệm bằng tiếng việt trong phần comment ở đầu file rồi mới thực hành
+// 4. Ví dụ cần cụ thể và rõ ràng hơn
